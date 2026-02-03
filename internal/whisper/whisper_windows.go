d4a118d1d0da35229c71f4cfb29698fb964c78f4
yakov <61390950+thewh1teagle@users.noreply.github.com>
2026-02-04 00:09:07 +0200

    fix window build

--- internal/whisper/whisper_windows.go
+++ internal/whisper/whisper_windows.go
@@ -6,7 +6,7 @@ package whisper
 #cgo CFLAGS: -I${SRCDIR}/../../third_party/include
 #cgo LDFLAGS: -L${SRCDIR}/../../third_party/lib
 #cgo LDFLAGS: -lwhisper -lggml -lggml-base -lggml-cpu -lggml-vulkan
-#cgo LDFLAGS: -lvulkan-1 -lstdc++ -lm -lpthread
+#cgo LDFLAGS: -lvulkan-1 -lstdc++ -lm -lpthread -lgomp
 #include <whisper.h>
 #include <stdlib.h>
 */
